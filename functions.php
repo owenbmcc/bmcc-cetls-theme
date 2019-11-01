26f981f340eff4922c8c20f451be0fa307bdae7c
Owen Roberts <oroberts@bmcc.cuny.edu>
2019-11-01 18:32:17 -0400

    testing

--- functions.php
+++ functions.php
@@ -1,5 +1,49 @@
 <?php
 
+/* custom project type */
+function create_post_type() {
+  $labels = array(
+    'name'               => 'Projects',
+    'singular_name'      => 'Project',
+    'menu_name'          => 'Projects',
+    'name_admin_bar'     => 'Project',
+    'add_new'            => 'Add New',
+    'add_new_item'       => 'Add New Project',
+    'new_item'           => 'New Project',
+    'edit_item'          => 'Edit Project',
+    'view_item'          => 'View Project',
+    'all_items'          => 'All Projects',
+    'search_items'       => 'Search Projects',
+    'parent_item_colon'  => 'Parent Project',
+    'not_found'          => 'No Projects Found',
+    'not_found_in_trash' => 'No Projects Found in Trash'
+  );
+
+  $args = array(
+    'labels'              => $labels,
+    'public'              => true,
+    'exclude_from_search' => false,
+    'publicly_queryable'  => true,
+    'show_ui'             => true,
+    'show_in_nav_menus'   => true,
+    'show_in_menu'        => true,
+    'show_in_admin_bar'   => true,
+    'menu_position'       => 5,
+    'menu_icon'           => 'dashicons-admin-appearance',
+    'capability_type'     => 'post',
+    'hierarchical'        => false,
+    'supports'            => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
+    'has_archive'         => true,
+    'rewrite'             => array( 'slug' => 'projects' ),
+    'query_var'           => true
+    );
+
+  register_post_type( 'sm_project', $args );
+}
+add_action( 'init', 'create_post_type' );
+
+
+
 function canvas_parent_theme_enqueue_styles() {
     wp_enqueue_style( 'canvas-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'bmcc-acert-style',
